2e91c928e7dbad66c4e2f69eaca82d6a38f97205
Tammy L Stonez <livingstoneowuf@gmail.com>
2026-05-06 01:51:00 +0100

    Add Netlify function to serve firebase config from env vars

---
+++ netlify/functions/firebase-config.js
@@ -0,0 +1,26 @@
+exports.handler = async function () {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/javascript",
+    },
+    body: `
+        import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+        import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+        import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+        const firebaseConfig = {
+        apiKey:            "${process.env.FIREBASE_API_KEY}",
+        authDomain:        "${process.env.FIREBASE_AUTH_DOMAIN}",
+        projectId:         "${process.env.FIREBASE_PROJECT_ID}",
+        storageBucket:     "${process.env.FIREBASE_STORAGE_BUCKET}",
+        messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID}",
+        appId:             "${process.env.FIREBASE_APP_ID}"
+        };
+
+        const app  = initializeApp(firebaseConfig);
+        export const auth = getAuth(app);
+        export const db   = getFirestore(app);
+    `.trim(),
+  };
+};
